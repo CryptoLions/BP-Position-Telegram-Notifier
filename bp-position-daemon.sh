58f6ced85e337aed3a4b53f9cae7ef9c71bd17a1
Bohdan Cryptolions <37701692+ansigroup@users.noreply.github.com>
2018-09-18 02:20:14 +0300

    Update bp-position-daemon.sh

--- bp-position-daemon.sh
+++ bp-position-daemon.sh
@@ -13,6 +13,7 @@
 
 
 #-- Config ------------------------------
+LAST_POSITION=50;
 
 # for what name moniotr position
 PRODUCER_NAME_CHECK="cryptolions1"
@@ -47,73 +48,102 @@ sendmessage(){
 }
 
 
+getVoteWeight(){
+    timestamp_epoch=946684800
+    now="$(date +%s)"
+    let "dates_=$now-$timestamp_epoch"
+    let "weight_=$dates_/(86400*7)"
+    weight=$(bc <<< "scale=16;$weight_/52")
+    res=$(bc -l <<< "e($weight*l(2))")
+    echo $res
+}
 #=====================================================================
 #=====================================================================
-
-LAST_POSITION=27;
+LAST_EOS_VOTES=0;
 
 while true; do
 
-    PROD_LIST=$($CLEOS system listproducers -l 50)
     DATE=`date -d "now" +'%Y.%m.%d %H:%M:%S'`
-    POSITION=-1;
+	POSITION=0;
 
-    while read -r line
-    do
-	#echo $line
-        POSITION=$(($POSITION+1))
+	PROD_LIST=$($CLEOS system listproducers -l 50 -j)
 
-	data=($line)
+	VOTE_WEIGHT=$(getVoteWeight)
+	TOTAL_VOTE_WEIGHT=$(echo $PROD_LIST | jq -r '.total_producer_vote_weight' | cut -f1 -d".")
 
-        NAME=${data[0]};
-        PROC=${data[${#data[@]}-1]};
+	for row in $(echo "${PROD_LIST}" | jq -r '.rows[] | @base64'); do
+		_jq() {
+			echo ${row} | base64 --decode | jq -r ${1}
+		}
 
-        MSG2="";
-        if [[ "$NAME" == "$PRODUCER_NAME_CHECK" ]]; then
+		NAME=$(_jq '.owner')
+		TOTAL_VOTES=$(_jq '.total_votes' | cut -f1 -d".")
+		PROC=$(bc <<< "scale=3; $TOTAL_VOTES*100/$TOTAL_VOTE_WEIGHT")
 
-	    if [[ $POSITION != $LAST_POSITION && $LAST_POSITION != -1 ]]; then
+		EOS_VOTES=$(bc <<< "scale=2; $TOTAL_VOTES/$VOTE_WEIGHT/10000")
+		EOS_VOTES_NICE=$(echo $EOS_VOTES | sed ':a;s/\B[0-9]\{3\}\>/ &/;ta')
 
-		SYMBOL="▲"
-		if [[ $POSITION > $LAST_POSITION ]]; then
-		    SYMBOL="▼"
+        	POSITION=$(($POSITION+1))
+
+        	MSG2="";
+        	if [[ "$NAME" == "$PRODUCER_NAME_CHECK" ]]; then
+	    		if [[ $POSITION != $LAST_POSITION && $LAST_POSITION != -1 ]]; then
+
+				SYMBOL="▲"
+				if [[ $POSITION > $LAST_POSITION ]]; then
+			    	SYMBOL="▼"
+				fi
+				MSG="$SYMBOL $DATE: Position Changed  $LAST_POSITION -> $POSITION - $PROC% %0A $EOS_VOTES_NICE EOS"
+
+
+				# in case you move to top 21 from standby
+	    		if [[ $LAST_POSITION -gt 21 && $POSITION -le 21 ]]; then
+			    	MSG2="✈ Be ready you are in top 21! You will start producing soon (in 2-3 rounds)"
+				fi
+
+				# in case you move out from top 21 to standby
+		    	if [[ $LAST_POSITION -le 21 && $POSITION -gt 21 ]]; then
+				    MSG2="💤 your node moved to stadnby"
+				fi
+
+				echo $MSG >> $LOG_FILE
+
+				sendmessage "$MSG"
+				if [[ "$MSG2" != "" ]]; then
+			    	echo $MSG2 >> $LOG_FILE
+			    	sleep 1
+			    	sendmessage "$MSG2"
+				fi
+				#echo "--" >> $LOG_FILE
+				LAST_POSITION=$POSITION
+				break
+		    fi
+
+			if [[ $LAST_EOS_VOTES == 0 ]]; then
+				LAST_EOS_VOTES=$EOS_VOTES;
+			fi
+
+		    if [[ $LAST_EOS_VOTES != $EOS_VOTES && $LAST_EOS_VOTES > 0 ]]; then
+    			DIFF=$(bc <<< "$EOS_VOTES - $LAST_EOS_VOTES");
+                if (( $(echo "$DIFF > 0" |bc -l) )); then
+					SYM="✚"
+                else
+                	SYM="▬ "
+                	DIFF=$(bc <<< "scale=2;-1*$DIFF")
+                fi
+
+                DIFF_NICE=$(echo $DIFF | sed ':a;s/\B[0-9]\{3\}\>/ &/;ta')
+
+                MSG="$SYM$DIFF_NICE EOS Votes = $EOS_VOTES_NICE EOS";
+                sendmessage "$MSG"
+
+    			LAST_EOS_VOTES=$EOS_VOTES
+		    fi
 		fi
-		PROC=$(echo $PROC*100 | bc)
-		MSG="$SYMBOL $DATE: Position Changed  $LAST_POSITION -> $POSITION  - $PROC %"
-
-
-		# in case you move to top 21 from standby
-    		if [[ $LAST_POSITION -gt 21 && $POSITION -le 21 ]]; then
-		    MSG2="✈ Be ready you are in top 21! You will start producing soon (in 2-3 rounds)"
-		fi
-
-		# in case you move out from top 21 to standby
-    		if [[ $LAST_POSITION -le 21 && $POSITION -gt 21 ]]; then
-		    MSG2="💤 your node moved to standby"
-		fi
-
-
-		echo $MSG >> $LOG_FILE
-
-
-		sendmessage "$MSG"
-		if [[ "$MSG2" != "" ]]; then
-		    echo $MSG2 >> $LOG_FILE
-		    sleep 1
-		    sendmessage "$MSG2"
-		fi
-		#echo "--" >> $LOG_FILE
-	    
-		LAST_POSITION=$POSITION
-
-		break
-	    fi
-	fi
-    done < <(printf '%s\n' "$PROD_LIST")
+	done
 
     sleep $TIME_BETWEEN_CHECKS;
 done
-
-
 
 
 
